69a9d02989535b5079bba3515d4dbe7994cc0322
Francesca Artes <122511777+francescaartes@users.noreply.github.com>
2026-04-30 22:09:10 +0800

    feat(config): add support for VITE_API_URL in production

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const isDev = import.meta.env.DEV;
+
 const api = axios.create({
-    baseURL: "/api",
+    baseURL: isDev ? "/api" : import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
 
